8b6f74d6e78da19d11840c434a5156e8ab13f20a
jayhooney <hooney731@gmail.com>
2021-01-01 05:28:33 +0900

    refactor: 코드 정리
    urlPattern에 맞춰 method, path params, query를 수정함

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -7,10 +7,9 @@ import { Response } from 'express';
 export class MovieService {
   private readonly logger = new Logger(MovieService.name);
   public addMovie(addMovieData: MovieDTO.AddMovieDTO, res: Response) {
-    this.logger.debug(addMovieData);
-    const db: Database = new Database();
     const queryItems: any[] = Object.values(addMovieData);
     const query = `insert into movie_tb (opening_date, title, grade, still_shots, description, modifier, update_dt) values (?,?,?,?,?,?,NOW());`;
+    const db: Database = new Database();
     db.ExecuteQuery(query, queryItems, (err, results, fields) => {
       res.send(results);
     });
@@ -28,11 +27,23 @@ export class MovieService {
     return;
   }
 
-  public deleteMovie() {
+  public deleteMovie(id: number, res: Response) {
+    const queryItems: any[] = [id];
+    const query = `delete from movie_tb where seq = ?`;
+    const db: Database = new Database();
+    db.ExecuteQuery(query, queryItems, (err, results, fileds) => {
+      res.send(results);
+    });
     return;
   }
 
-  public updateMovie() {
+  public updateMovie(id: number, res: Response) {
+    const queryItems: any[] = [id];
+    const query = `update movie_tb set title = '' where seq = ?;`;
+    const db: Database = new Database();
+    db.ExecuteQuery(query, queryItems, (err, results, fileds) => {
+      res.send(results);
+    });
     return;
   }
 }
